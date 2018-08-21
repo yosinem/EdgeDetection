b59ba021066f0ec8c575f71bde5174952af8aa10
yosi <yosinem@gmail.com>
2018-08-22 02:48:00 +0300

    init1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,12 @@ import boofcv.io.UtilIO;
 import boofcv.io.image.UtilImageIO;
 import georegression.struct.point.Point2D_I32;
 import imageprocessing.EdgeDetection;
+import imageprocessing.Square;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,39 +16,70 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        File originalImage = new File("C:\\Users\\yosi\\Desktop\\card-png\\pekka.png");
+        File cropedImage = new File("C:\\Users\\yosi\\Desktop\\images\\35.jpg");
+        float precentege = compareImage(originalImage,cropedImage);
 
         BufferedImage image = UtilImageIO.loadImage(UtilIO.pathExample("C:\\Users\\yosi\\Downloads\\Clash_Royale.jpg"));
         List<Contour> contours = EdgeDetection.getEdges(Image.path);
 
-        List<Rectangle> squareContours = convertContoursToRectangles(contours);
+        List<Square> squareContours = convertContoursToSquares(contours);
 
-
-    }
-
-    private static List<Rectangle> convertContoursToRectangles(List<Contour> contours) {
-        List<Rectangle> rectangles = new ArrayList<Rectangle>();
-        for (Contour contour:contours){
-            Rectangle rectangle = createRectangleFromContour(contour.external);
-            rectangles.add(rectangle);
+        for (int i = 0; i < 100; i++) {
+            cropImage(image,squareContours.get(i),i);
         }
-        return rectangles;
     }
 
-    private static Rectangle createRectangleFromContour(List<Point2D_I32> points) {
-        int minX = ContourUtil.getMinX(points);
-        int minY = ContourUtil.getMinY(points);
-        int width = ContourUtil.getWidth(points,minX);
-        int height = ContourUtil.getHeight(points,minY);
-
-        return new Rectangle(minX,minY,width,height);
+    private static List<Square> convertContoursToSquares(List<Contour> contours) {
+        List<Square> squares = new ArrayList<Square>();
+        for (Contour contour:contours){
+            Square square = new Square(contour.external);
+            squares.add(square);
+        }
+        return squares;
     }
 
 
-    private static File cropImage(BufferedImage src, Rectangle rectangle) throws IOException {
-        BufferedImage dest = src.getSubimage(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-        File outputFile = new File("image.jpg");
-        ImageIO.write(dest, "jpg", outputFile);
+    private static File cropImage(BufferedImage src, Square square, int fileName) throws IOException {
+        BufferedImage dest = src.getSubimage(square.getMinX(), square.getMinY(), square.getWidth(), square.getHeight());
+
+        File outputFile = new File("C:\\Users\\yosi\\Desktop\\images\\"+ fileName +".png");
+        ImageIO.write(dest, "png", outputFile);
         return outputFile;
+    }
+
+    public static float compareImage(File fileA, File fileB) {
+
+        float percentage = 0;
+        try {
+            // take buffer data from both image files //
+            BufferedImage biA = ImageIO.read(fileA);
+            DataBuffer dbA = biA.getData().getDataBuffer();
+            int sizeA = dbA.getSize();
+            BufferedImage biB = ImageIO.read(fileB);
+            DataBuffer dbB = biB.getData().getDataBuffer();
+            int sizeB = dbB.getSize();
+            int count = 0;
+            // compare data-buffer objects //
+            if (sizeA == sizeB) {
+
+                for (int i = 0; i < sizeA; i++) {
+
+                    if (dbA.getElem(i) == dbB.getElem(i)) {
+                        count = count + 1;
+                    }
+
+                }
+                percentage = (count * 100) / sizeA;
+            } else {
+                System.out.println("Both the images are not of same size");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Failed to compare image files ...");
+        }
+        return percentage;
     }
 }
--- src/main/java/imageprocessing/Square.java
+++ src/main/java/imageprocessing/Square.java
@@ -1,4 +1,56 @@
 package imageprocessing;
 
+import georegression.struct.point.Point2D_I32;
+
+import java.util.List;
+
 public class Square {
+
+    private int minX;
+    private int minY;
+    private int maxX;
+    private int maxY;
+
+    public Square(List<Point2D_I32> points) {
+
+        minY = 5000;
+        minX = 5000;
+        for (Point2D_I32 point : points) {
+            int y = point.getY();
+            int x = point.getX();
+
+            if (y > maxY) {
+                maxY = y;
+            }
+
+            if (y < minY) {
+                minY = y;
+            }
+
+            if (x > maxX){
+                maxX = x;
+            }
+
+            if (x < minX){
+                minX = x;
+            }
+        }
+    }
+
+    public int getMinX() {
+        return minX;
+    }
+
+    public int getMinY() {
+        return minY;
+    }
+
+    public int getWidth(){
+        return maxX - minX;
+    }
+
+    public int getHeight(){
+        return maxY - minY;
+
+    }
 }
